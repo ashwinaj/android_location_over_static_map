8b76c2f4e080ac559d9128d42cbe582b1fccdd55
Karunakaran Prasad <kprasad@Karunakarans-MacBook-Air.local>
2016-10-20 03:58:53 -0700

    implemented multi touch

--- app/src/main/java/com/nirupama/prasad/mapmysjsu/MapActivity.java
+++ app/src/main/java/com/nirupama/prasad/mapmysjsu/MapActivity.java
@@ -1,6 +1,7 @@
 package com.nirupama.prasad.mapmysjsu;
 
 import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
@@ -8,6 +9,7 @@ import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.FloatMath;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
@@ -15,7 +17,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements View.OnTouchListener{
 
     //In order to make the map pinch zoomable
     ImageView mapImageView;
@@ -24,16 +26,115 @@ public class MapActivity extends AppCompatActivity {
     ScaleGestureDetector mapScaleGestureDetector;
 
 
+    // these matrices will be used to move and zoom image
+    private Matrix matrix = new Matrix();
+    private Matrix savedMatrix = new Matrix();
+    // we can be in one of these 3 states
+    private static final int NONE = 0;
+    private static final int DRAG = 1;
+    private static final int ZOOM = 2;
+    private int mode = NONE;
+    // remember some things for zooming
+    private PointF start = new PointF();
+    private PointF mid = new PointF();
+    private float oldDist = 1f;
+    private float d = 0f;
+    private float newRot = 0f;
+    private float[] lastEvent = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
         mapImageView = (ImageView) findViewById(R.id.mapImageView);
-        mapScaleGestureDetector = new ScaleGestureDetector(this, new MapScaleListener());
+        mapImageView.setOnTouchListener(this);
+        //mapScaleGestureDetector = new ScaleGestureDetector(this, new MapScaleListener());
 
         CenterMapImage();
 
+    }
+
+    public boolean onTouch(View v, MotionEvent event) {
+        // handle touch events here
+        ImageView view = (ImageView) v;
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                mode = DRAG;
+                lastEvent = null;
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                oldDist = spacing(event);
+                if (oldDist > 10f) {
+                    savedMatrix.set(matrix);
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                }
+                lastEvent = new float[4];
+                lastEvent[0] = event.getX(0);
+                lastEvent[1] = event.getX(1);
+                lastEvent[2] = event.getY(0);
+                lastEvent[3] = event.getY(1);
+                d = rotation(event);
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+                mode = NONE;
+                lastEvent = null;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mode == DRAG) {
+                    matrix.set(savedMatrix);
+                    float dx = event.getX() - start.x;
+                    float dy = event.getY() - start.y;
+                    matrix.postTranslate(dx, dy);
+                } else if (mode == ZOOM) {
+                    float newDist = spacing(event);
+                    if (newDist > 10f) {
+                        matrix.set(savedMatrix);
+                        float scale = (newDist / oldDist);
+                        matrix.postScale(scale, scale, mid.x, mid.y);
+                    }
+                    if (lastEvent != null && event.getPointerCount() == 3) {
+                        newRot = rotation(event);
+                        float r = newRot - d;
+                        float[] values = new float[9];
+                        matrix.getValues(values);
+                        float tx = values[2];
+                        float ty = values[5];
+                        float sx = values[0];
+                        float xc = (view.getWidth() / 2) * sx;
+                        float yc = (view.getHeight() / 2) * sx;
+                        matrix.postRotate(r, tx + xc, ty + yc);
+                    }
+                }
+                break;
+        }
+
+        view.setImageMatrix(matrix);
+        return true;
+    }
+
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float) Math.sqrt(x * x + y * y);
+    }
+
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
+
+    private float rotation(MotionEvent event) {
+        double delta_x = (event.getX(0) - event.getX(1));
+        double delta_y = (event.getY(0) - event.getY(1));
+        double radians = Math.atan2(delta_y, delta_x);
+        return (float) Math.toDegrees(radians);
     }
 
     private void CenterMapImage() {
@@ -54,7 +155,7 @@ public class MapActivity extends AppCompatActivity {
         mapImageView.setImageMatrix(mapMatrix);
     }
 
-    private class MapScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+   /* private class MapScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
 
@@ -73,5 +174,5 @@ public class MapActivity extends AppCompatActivity {
     public boolean onTouchEvent(MotionEvent event) {
         mapScaleGestureDetector.onTouchEvent(event);
         return true;
-    }
+    }*/
 }
