5cff1875fb0ff003610a864846be73a4fa84d239
Karunakaran Prasad <kprasad@Karunakarans-MacBook-Air.local>
2016-10-20 02:16:42 -0700

    Centering matrix map

--- app/src/main/java/com/nirupama/prasad/mapmysjsu/MapActivity.java
+++ app/src/main/java/com/nirupama/prasad/mapmysjsu/MapActivity.java
@@ -1,11 +1,19 @@
 package com.nirupama.prasad.mapmysjsu;
 
 import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -23,13 +31,35 @@ public class MapActivity extends AppCompatActivity {
 
         mapImageView = (ImageView) findViewById(R.id.mapImageView);
         mapScaleGestureDetector = new ScaleGestureDetector(this, new MapScaleListener());
+
+        mapImageView.setScaleType(ImageView.ScaleType.MATRIX);
+
+
+        //Get image dimensions
+        Drawable mapDrawable = mapImageView.getDrawable();
+        float imageWidth = mapDrawable.getIntrinsicWidth();
+        float imageHeight = mapDrawable.getIntrinsicHeight();
+
+        //Get Screen dimensions
+        DisplayMetrics metrics = this.getResources().getDisplayMetrics();
+        int screenWidth = metrics.widthPixels;
+        int screenHeight = metrics.heightPixels;
+
+        RectF mapDrawableRect = new RectF(0, 0, imageWidth, imageHeight);
+        RectF viewImageRect = new RectF(0, 0, screenWidth, screenHeight);
+        mapMatrix.setRectToRect(mapDrawableRect, viewImageRect, Matrix.ScaleToFit.CENTER);
+        mapImageView.setImageMatrix(mapMatrix);
+
     }
 
-    private class MapScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener{
+    private class MapScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
-            mapInitScale = mapInitScale * detector.getScaleFactor();
-            mapInitScale = Math.max(0.1f, Math.min(mapInitScale, 5f));
+
+            float newScaleFactor = detector.getScaleFactor();
+            mapInitScale = newScaleFactor * mapInitScale;
+            mapInitScale = Math.max(1f, Math.min(mapInitScale, 5f));
+            Log.d("MapActivity", "MapScaleListener, initscale = " + mapInitScale + " new scale = " + newScaleFactor);
             mapMatrix.setScale(mapInitScale, mapInitScale);
 
             mapImageView.setImageMatrix(mapMatrix);
